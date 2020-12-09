bef753e58af07b92f796a0d6ef0a6256e7cf1b10
viowla <55015538+viowla@users.noreply.github.com>
2020-12-09 14:40:49 +0600

    connect activity and layout accordingly

--- app/src/main/java/com/example/parttimejobapplication/RegistrationActivity.java
+++ app/src/main/java/com/example/parttimejobapplication/RegistrationActivity.java
@@ -9,5 +9,6 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
     }
 }
