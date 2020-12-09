216f2dc8761ca9c6450efb86fc658e76a417c3cb
viowla <55015538+viowla@users.noreply.github.com>
2020-12-09 14:35:35 +0600

    add login and registration activities

---
+++ app/src/main/java/com/example/parttimejobapplication/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.example.parttimejobapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
