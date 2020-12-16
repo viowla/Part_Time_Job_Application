835ab7481877b25fdc5afb8e3efe5f1c4bffb3f5
viowla <55015538+viowla@users.noreply.github.com>
2020-12-16 18:06:51 +0600

    add notification, favorite, chat pages

--- app/src/main/java/com/example/parttimejobapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/parttimejobapplication/ui/home/HomeFragment.java
@@ -19,7 +19,7 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.activity_first_page, container, false);
 
         return root;
     }
